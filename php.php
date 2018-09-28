34d728e363829dc5efc741e69dc0b8bc2d984906
GMora13 <jose.mora_13@outlook.com>
2018-09-27 19:50:18 -0600

    Primer Commit

---
+++ php.php
@@ -0,0 +1,15 @@
+<?php
+    $strMensaje = "Hola mundo";
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Ejemplo de PHP</title>
+   </head>
+   <body>
+     <h1><?php echo $strMensaje; ?></h1>
+
+   </body>
+ </html>
